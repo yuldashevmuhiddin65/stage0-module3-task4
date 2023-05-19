5cd188e1d4212e20e385846aaca16d7d955ff998
m.yuldashev <yuldashevmuhiddin65@gmail.com>
2023-05-19 16:46:06 +0500

    module 3 task 4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a, b, result;
+        a = 2;
+        b = 5;
+
+        result = (9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2;
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 'c';
+        int h = 'h';
+        int a = 'a';
+        int r = 'r';
+        int s = 's';
+
     }
 }
