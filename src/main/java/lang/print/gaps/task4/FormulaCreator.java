30b950bed2e87f3bc906478b92ea50ea4b382cbd
yuldashevmuhiddin65 <102411263+yuldashevmuhiddin65@users.noreply.github.com>
2023-05-20 18:26:21 +0500

    I`ve fixed bugs!

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,11 +2,11 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a, b, result;
-        a = 2;
-        b = 5;
+        Double result;
+        double a = 2;
+        double b = 5;
 
-        result = (9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2;
+        result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
         System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,11 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = 'c';
-        int h = 'h';
-        int a = 'a';
-        int r = 'r';
-        int s = 's';
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+        System.out.println(c);
+
+
 
     }
 }
